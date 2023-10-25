f4e92b5598268b43fdfd121a1f7cb67bd810dc2c
xiahuiliu <coffeeliuxiahui@gmail.com>
2023-10-25 16:22:09 +0200

    change draw one card to draw 4 cards

--- drawAcard/index.js
+++ drawAcard/index.js
@@ -1,47 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('myBtn');
 
-    button.addEventListener('click', (event) => {
-        event.preventDefault();
-        const url = 'https://deckofcardsapi.com/api/deck/new/draw/?count=1';
-        const displayInfo = (data) => {
-            const div = document.getElementById('display');
+document.getElementById('myBtn').addEventListener('click', async function() {
+    // get deck id
+    const response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+    const data = await response.json();
+    const deckId = data.deck_id;
+    // use deck id to draw cards
+    const drawResponse = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`);
+    const drawData = await drawResponse.json();
+    const drawnCards = drawData.cards;
 
-            // create a div to hold each card
-            const cardDiv = document.createElement('div');
-            cardDiv.classList.add('card');
-            div.appendChild(cardDiv);
+    const cardsContainer = document.getElementById('cardsContainer');
+    cardsContainer.innerHTML = '';
 
-            // create a h2 element for the card
-            const h2 = document.createElement('h2');
-            h2.innerHTML = `${data.suit} ${data.value}`;
-            cardDiv.appendChild(h2);
-
-            // create an img element for the card
-            const img = document.createElement('img');
-            const imgAtt = document.createAttribute("src");
-            imgAtt.value = `${data.image}`;
-            cardDiv.appendChild(img).setAttributeNode(imgAtt);  
-        }
-        const getData = async(url) => {
-            try {
-                const res = await fetch(url);
-                if (!res.ok) {
-                    throw new Error(`HTTP error! status: ${res.status}`);
-                } 
-                const data = await res.json();
-                return data;
-            } catch(error){
-                console.error('Error:', error);
-                throw error; 
-            }
-        }
-        getData(url)
-            .then(data => {
-                displayInfo(data.cards[0]);
-            })
-            .catch(error => {
-                console.error('Error', error);
-            })
-    })
-})
+    drawnCards.forEach(card => {
+        const cardElement = document.createElement('img');
+        cardElement.classList.add('card');
+        cardElement.src = card.image;
+        cardElement.alt = `${card.value} of ${card.suit}`;
+        cardsContainer.appendChild(cardElement);
+    });
+});
